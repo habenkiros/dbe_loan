ecb32b9a08cccd70d2c02fc2f0b884ca5558290d
habenkiros <habenhkt@gmail.com>
2026-01-08 18:12:04 +0300

    Update settings.py

--- decsi_loan/settings.py
+++ decsi_loan/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
-DEBUG = os.getenv('DEBUG', 'Flase') == 'False'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
